7a5836683a8f0f24ed62ec0836eea055e8deb249
HemaShri Kodeboyina <hemashri.kodeboyina@gmail.com>
2021-09-06 00:22:24 +0530

    local commit

---
+++ src/main/java/com/testingwebhook/TestingWebhookApplication.java
@@ -0,0 +1,13 @@
+package com.testingwebhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingWebhookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingWebhookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testingwebhook/TestingWebhookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testingwebhook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingWebhookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
